445f62845bc43676ead67ac19d9a8cf0cc62da6f
phlong3105 <longpham3105@gmail.com>
2022-05-20 11:41:41 +0900

    Add supports for iOS (Swift) projects

---
+++ src/swift/one.h
@@ -0,0 +1,18 @@
+//
+//  one.h
+//  one
+//
+//  Created by Long H. Pham on 2022/05/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for one.
+FOUNDATION_EXPORT double oneVersionNumber;
+
+//! Project version string for one.
+FOUNDATION_EXPORT const unsigned char oneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <one/PublicHeader.h>
+
+
