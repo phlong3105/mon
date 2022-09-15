c36fb904ecd2571b8d33e75c8cd3ee30a0d9ad70
longpham <longpham3105@gmail.com>
2022-09-15 16:44:29 +0900

    Re-organize project files

--- src/one/nn/layer.py
+++ src/one/nn/layer.py
@@ -3494,6 +3494,82 @@ class InceptionBasicConv2d(Module):
     def forward(self, input: Tensor) -> Tensor:
         return self.act(self.bn(self.conv(input)))
     
+    
+@LAYERS.register(name="inception")
+class Inception(Module):
+    
+    def __init__(
+        self,
+        in_channels: int,
+        ch1x1      : int,
+        ch3x3red   : int,
+        ch3x3      : int,
+        ch5x5red   : int,
+        ch5x5      : int,
+        pool_proj  : int,
+        conv_block : Callable[..., nn.Module] | None = None,
+    ):
+        super().__init__()
+        if conv_block is None:
+            conv_block = InceptionBasicConv2d
+        
+        self.branch1 = conv_block(
+            in_channels  = in_channels,
+            out_channels = ch1x1,
+            kernel_size  = 1,
+        )
+        self.branch2 = Sequential(
+            conv_block(
+                in_channels  = in_channels,
+                out_channels = ch3x3red,
+                kernel_size  = 1,
+            ),
+            conv_block(
+                in_channels  = ch3x3red,
+                out_channels = ch3x3,
+                kernel_size  = 3,
+                padding      = 1,
+            )
+        )
+        self.branch3 = Sequential(
+            conv_block(
+                in_channels  = in_channels,
+                out_channels = ch5x5red,
+                kernel_size  = 1,
+            ),
+            # Here, kernel_size=3 instead of kernel_size=5 is a known bug.
+            # Please see https://github.com/pytorch/vision/issues/906 for
+            # details.
+            conv_block(
+                in_channels  = ch5x5red,
+                out_channels = ch5x5,
+                kernel_size  = 3,
+                padding      = 1,
+            ),
+        )
+        self.branch4 = Sequential(
+            MaxPool2d(
+                kernel_size = 3,
+                stride      = 1,
+                padding     = 1,
+                ceil_mode   = True,
+            ),
+            conv_block(
+                in_channels  = in_channels,
+                out_channels = pool_proj,
+                kernel_size  = 1,
+            ),
+        )
+
+    def forward(self, input: Tensor) -> Tensor:
+        x       = input
+        branch1 = self.branch1(x)
+        branch2 = self.branch2(x)
+        branch3 = self.branch3(x)
+        branch4 = self.branch4(x)
+        outputs = [branch1, branch2, branch3, branch4]
+        return torch.cat(outputs, dim=1)
+
 
 @LAYERS.register(name="inception_a")
 class InceptionA(Module):
@@ -3854,8 +3930,8 @@ class InceptionE(Module):
         return torch.cat(outputs, 1)
 
 
-@LAYERS.register(name="inception_aux")
-class InceptionAux(Module):
+@LAYERS.register(name="inception_aux1")
+class InceptionAux1(Module):
     
     def __init__(
         self,
@@ -3899,6 +3975,47 @@ class InceptionAux(Module):
         # N x 1000
         return x
 
+
+@LAYERS.register(name="inception_aux2")
+class InceptionAux2(Module):
+    
+    def __init__(
+        self,
+        in_channels : int,
+        out_channels: int,
+        dropout     : float                           = 0.7,
+        conv_block  : Callable[..., nn.Module] | None = None,
+    ):
+        super().__init__()
+        if conv_block is None:
+            conv_block = InceptionBasicConv2d
+       
+        self.conv = conv_block(
+            in_channels  = in_channels,
+            out_channels = 128,
+            kernel_size  = 1,
+        )
+        self.fc1     = Linear(in_features=2048, out_features=1024)
+        self.fc2     = Linear(in_features=1024, out_features=out_channels)
+        self.dropout = Dropout(p=dropout)
+
+    def forward(self, input: Tensor) -> Tensor:
+        x = input
+        # aux1: N x 512 x 14 x 14, aux2: N x 528 x 14 x 14
+        x = F.adaptive_avg_pool2d(x, (4, 4))
+        # aux1: N x 512 x 4 x 4, aux2: N x 528 x 4 x 4
+        x = self.conv(x)
+        # N x 128 x 4 x 4
+        x = torch.flatten(x, 1)
+        # N x 2048
+        x = F.relu(self.fc1(x), inplace=True)
+        # N x 1024
+        x = self.dropout(x)
+        # N x 1024
+        x = self.fc2(x)
+        # N x 1000 (num_classes)
+        return x
+    
 
 # H2: - MBLLEN -----------------------------------------------------------------
 
