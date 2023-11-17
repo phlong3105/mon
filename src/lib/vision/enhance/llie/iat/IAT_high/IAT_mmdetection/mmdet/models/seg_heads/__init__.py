c6cc18806f490741bcc4d9a5b04f260b92080fe1
longpham <longpham3105@gmail.com>
2023-11-17 14:07:08 +0900

    Refactor code

--- src/lib/vision/enhance/llie/iat/IAT_high/IAT_mmdetection/mmdet/models/seg_heads/__init__.py
+++
@@ -1,2 +0,0 @@
-from .panoptic_fpn_head import PanopticFPNHead  # noqa: F401,F403
-from .panoptic_fusion_heads import *  # noqa: F401,F403
--- src/lib/vision/enhance/llie/iat/IAT_high/IAT_mmdetection/mmdet/models/seg_heads/panoptic_fusion_heads/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_panoptic_fusion_head import \
-    BasePanopticFusionHead  # noqa: F401,F403
-from .heuristic_fusion_head import HeuristicFusionHead  # noqa: F401,F403
