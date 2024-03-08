ed1d024d58420fd2299802a1d4a329e3348bf4db
longpham <longpham3105@gmail.com>
2024-03-08 15:12:17 +0900

    Refactor

--- src/mon_lib/vision/multitask/ultralytics_8_0_29/ultralytics/yolo/v8/segment/__init__.py
+++
@@ -1,5 +0,0 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
-
-from .predict import predict, SegmentationPredictor
-from .train import SegmentationTrainer, train
-from .val import SegmentationValidator, val
