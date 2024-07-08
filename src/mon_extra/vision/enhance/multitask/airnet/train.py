93b53f6334dd0e68b0fc5cd0120b38740fb2b7ee
longpham <longpham3105@gmail.com>
2024-07-08 16:19:12 +0900

    Refactor

--- src/mon_extra/vision/enhance/multitask/airnet/train.py
+++ src/mon_extra/vision/enhance/multitask/airnet/train.py
@@ -1,23 +1,22 @@
 import subprocess
-from tqdm import tqdm
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
-from utils.dataset_utils import TrainDataset
 from net.model import AirNet
-
 from option import options as opt
+from utils.dataset_utils import TrainDataset
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     torch.cuda.set_device(opt.cuda)
-    subprocess.check_output(['mkdir', '-p', opt.ckpt_path])
+    subprocess.check_output(["mkdir", "-p", opt.ckpt_path])
 
-    trainset = TrainDataset(opt)
-    trainloader = DataLoader(trainset, batch_size=opt.batch_size, pin_memory=True, shuffle=True,
-                             drop_last=True, num_workers=opt.num_workers)
+    trainset    = TrainDataset(opt)
+    trainloader = DataLoader(trainset, batch_size=opt.batch_size, pin_memory=True, shuffle=True, drop_last=True, num_workers=opt.num_workers)
 
     # Network Construction
     net = AirNet(opt).cuda()
@@ -29,7 +28,7 @@ if __name__ == '__main__':
     l1 = nn.L1Loss().cuda()
 
     # Start training
-    print('Start training...')
+    print("Start training...")
     for epoch in range(opt.epochs):
         for ([clean_name, de_id], degrad_patch_1, degrad_patch_2, clean_patch_1, clean_patch_2) in tqdm(trainloader):
             degrad_patch_1, degrad_patch_2 = degrad_patch_1.cuda(), degrad_patch_2.cuda()
@@ -52,33 +51,27 @@ if __name__ == '__main__':
             optimizer.step()
 
         if epoch < opt.epochs_encoder:
-            print(
-                'Epoch (%d)  Loss: contrast_loss:%0.4f\n' % (
-                    epoch, contrast_loss.item(),
-                ), '\r', end='')
+            print("Epoch (%d)  Loss: contrast_loss:%0.4f\n" % (epoch, contrast_loss.item(),), "\r", end='')
         else:
-            print(
-                'Epoch (%d)  Loss: l1_loss:%0.4f contrast_loss:%0.4f\n' % (
-                    epoch, l1_loss.item(), contrast_loss.item(),
-                ), '\r', end='')
+            print("Epoch (%d)  Loss: l1_loss:%0.4f contrast_loss:%0.4f\n" % (epoch, l1_loss.item(), contrast_loss.item(),), "\r", end="")
 
         GPUS = 1
         if (epoch + 1) % 50 == 0:
             checkpoint = {
-                "net": net.state_dict(),
-                'optimizer': optimizer.state_dict(),
-                "epoch": epoch
+                "net"      : net.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "epoch"    : epoch
             }
             if GPUS == 1:
-                torch.save(net.state_dict(), opt.ckpt_path + 'epoch_' + str(epoch + 1) + '.pth')
+                torch.save(net.state_dict(), opt.ckpt_path + "epoch_" + str(epoch + 1) + ".pth")
             else:
-                torch.save(net.module.state_dict(), opt.ckpt_path + 'epoch_' + str(epoch + 1) + '.pth')
+                torch.save(net.module.state_dict(), opt.ckpt_path + "epoch_" + str(epoch + 1) + ".pth")
 
         if epoch <= opt.epochs_encoder:
             lr = opt.lr * (0.1 ** (epoch // 60))
             for param_group in optimizer.param_groups:
-                param_group['lr'] = lr
+                param_group["lr"] = lr
         else:
             lr = 0.0001 * (0.5 ** ((epoch - opt.epochs_encoder) // 125))
             for param_group in optimizer.param_groups:
-                param_group['lr'] = lr
+                param_group["lr"] = lr
